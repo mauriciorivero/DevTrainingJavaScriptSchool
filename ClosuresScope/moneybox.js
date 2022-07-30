15a8eb66058b48ff2f3898cfa9743cf9ec81b2ae
Mauricio Rivero <rivero.mauricio@gmail.com>
2022-07-30 18:08:41 -0500

    added the functionality that has the error in the bussines logic fo the closure excercise

--- ClosuresScope/moneybox.js
+++ ClosuresScope/moneybox.js
@@ -1,19 +1,28 @@
 const myMoneyBox = moneyBoxClosure();
+
+//this is the functionality for the button action in the "no closure scenario"
 document.getElementById("saveAmmountBtn").onclick = function(){
     let ammount = parseInt(document.getElementById("ammountInput").value);
     document.getElementById("ammountResult").innerHTML = "Your save today: "+ammount;
+    moneyBox(ammount);    
+}
+
+//this is the functionality for the button action in the "closure scenario"
+document.getElementById("saveAmmountBtn2").onclick = function(){
+    let ammount = parseInt(document.getElementById("ammountInput2").value);
+    document.getElementById("ammountResult2").innerHTML = "Your save today: "+ammount;
     myMoneyBox(ammount);
 }
+
 
 //this function doesn't work because is not remember or not saving the coins;
 function moneyBox(coins){
     let saveCoins = 0;
     saveCoins += coins;
     console.log(`MoneyBox: $${saveCoins}`);
+    document.getElementById("ammountTotal").innerHTML = "Your total save: "+saveCoins;
+    return saveCoins;
 }
-
-moneyBox(5);
-moneyBox(5);
 
 //This function does remember the coins because the logic has a closure for that
 function moneyBoxClosure(){
@@ -21,7 +30,7 @@ function moneyBoxClosure(){
     function countCoins(coins){
         saveCoins += coins;
         console.log(`MoneyBoxClosure: $${saveCoins}`);
-        document.getElementById("ammountTotal").innerHTML = "Your total save: "+saveCoins;
+        document.getElementById("ammountTotal2").innerHTML = "Your total save: "+saveCoins;
     }
     return countCoins;
 }
